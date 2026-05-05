c4e0cc7685c94cacc453e60e5ea0441116d0a2c1
Pete Kubiak <peter.s.kubiak@bath.edu>
2026-05-05 23:47:12 +0100

    Bring up hello world

--- examples/microbit-v2/src/main.rs
+++ examples/microbit-v2/src/main.rs
@@ -1,2 +1,18 @@
 #![no_std]
 #![no_main]
+
+use panic_probe as _;
+
+use embassy_executor::Spawner;
+use rtt_target::rprintln;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    rtt_target::rtt_init_print!();
+    let _peripherals = embassy_nrf::init(Default::default());
+
+    loop {
+        rprintln!("Hello, world!");
+        embassy_time::Timer::after_secs(1).await;
+    }
+}
