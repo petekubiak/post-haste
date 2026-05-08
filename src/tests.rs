a164f01413f9a0afe6b2688ffa126c8ff2344f34
Thomas Flaherty <thomas.flaherty@dyson.com>
2026-05-08 07:51:27 +0100

    move variant count test to top of tests module

--- src/tests.rs
+++ src/tests.rs
@@ -3,6 +3,19 @@ mod tests {
     use crate as post_haste;
 
     #[test]
+    fn variant_count() {
+        #[post_haste::addresses]
+        #[allow(unused)]
+        enum MyEnum {
+            Variant1,
+            Variant2,
+            Variant3,
+            Variant4,
+        }
+        assert_eq!(POSTMASTER_ADDRESSES_VARIANT_COUNT, 4);
+    }
+
+    #[test]
     fn normal_usage() {
         let t = trybuild::TestCases::new();
         t.pass("compile_tests/normal_usage.rs");
@@ -24,18 +37,5 @@ mod tests {
     fn non_enum_address() {
         let t = trybuild::TestCases::new();
         t.compile_fail("compile_tests/non_enum_address.rs");
-    }
-
-    #[test]
-    fn variant_count() {
-        #[post_haste::addresses]
-        #[allow(unused)]
-        enum MyEnum {
-            Variant1,
-            Variant2,
-            Variant3,
-            Variant4,
-        }
-        assert_eq!(POSTMASTER_ADDRESSES_VARIANT_COUNT, 4);
     }
 }
